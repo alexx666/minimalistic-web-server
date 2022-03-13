5da753f91ae46b499873d3c999e5cbb9dec1b35b
Alexander <alex@spotlab.org>
2022-03-13 09:27:09 +0100

    feat: send static page from folder uses env variable for folder path

--- src/handler.c
+++ src/handler.c
@@ -1,19 +1,39 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// TODO: define as file in FS
-static const char *response =
-    "HTTP/1.1 200 OK\n"
-    "Server: MY_SERVER\n"
-    "Content-Type: text/html\n"
-    "Connection: Closed\n"
-    "\n"
-    "Hello madafaka!\n";
+#define FOLDER_PATH "FOLDER_PATH"
+#define INDEX "index.html"
 
-// TODO: define struct for request
+// TODO: pass parsed request
 void handleRequest(char *request, int *connection)
 {
+    char *folderpath = getenv(FOLDER_PATH);
+
+    char requestPath[100] = {0};
+
+    char response[1024] =
+        "HTTP/1.1 200 OK\n"
+        "Server: MY_SERVER\n"
+        "Content-Type: text/html\n"
+        "Connection: Closed\n"
+        "\n";
+
+    FILE *fp;
+
+    char buff[256] = {0};
+
+    strcat(requestPath, folderpath);
+    strcat(requestPath, INDEX); // TODO: append path from request
+
+    fp = fopen(requestPath, "r"); // TODO: send default error if not found
+
+    while (fgets(buff, sizeof(buff), (FILE *)fp) != NULL) strcat(response, buff);
+    
+    fclose(fp);
+
     send(*connection, response, strlen(response), 0);
+
     printf("Response sent\n------\n%s\n------\n", response);
 }
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 // TODO: read connection and return parsed request as struct
 char parseRequest(int *connection)
 {
-    char request[1024] = {0};
+    char request[10*1024] = {0};
     read(*connection, &request, sizeof(request));
     printf("Request received\n------\n%s\n------\n", request);
     return *request;
